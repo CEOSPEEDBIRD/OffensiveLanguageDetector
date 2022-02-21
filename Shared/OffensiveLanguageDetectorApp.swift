dca989e90496aa6f6f8218806a488924d70f0812
CEOSPEEDBIRD <ceo.uz@gmx.de>
2022-02-21 15:54:31 +0100

    Initial Commit

---
+++ Shared/OffensiveLanguageDetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  OffensiveLanguageDetectorApp.swift
+//  Shared
+//
+//  Created by Luca on 21/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct OffensiveLanguageDetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
