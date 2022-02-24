f241290690f8f2367c7e4cb3bdde0dd82b13f354
CEOSPEEDBIRD <ceo.uz@gmx.de>
2022-02-24 08:48:01 +0100

    Basic UI completed

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,14 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var textInput = String()
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                ForEach(1..<8) { Message in
+                    Label("Message", systemImage: "xmark.octagon.fill")
+                        .symbolRenderingMode(.multicolor)
+                    Label("Message", systemImage: "checkmark.seal.fill")
+                        .symbolRenderingMode(.palette)
+                        .foregroundStyle(.white, .green)
+                }
+            }.listStyle(SidebarListStyle())
+                .safeAreaInset(edge: .bottom) {
+                    inputBar
+                }
+        }
+    }
+    var inputBar: some View {
+        HStack {
+            TextField("Your Message",text: $textInput)
+                .padding(2)
+                .textFieldStyle(.roundedBorder)
+            Button {
+                
+            } label: {
+                Image(systemName: "paperplane.circle.fill")
+                    .renderingMode(.original)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(maxHeight: 30)
+                    .padding(5)
+            }
+        }.padding(.horizontal)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
     }
 }
--- Shared/OffensiveLanguageDetectorApp.swift
+++ Shared/OffensiveLanguageDetectorApp.swift
@@ -12,6 +12,7 @@ struct OffensiveLanguageDetectorApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .preferredColorScheme(.dark)
         }
     }
 }
